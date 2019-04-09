0ad6f19078d83bf51055c56ca5d5437fe74dc154
Jose H <13516027@std.stei.itb.ac.id>
2019-04-09 15:40:27 +0700

    Fix pusher and puller

--- Assets/Bermuda/Scripts/BERMUDA/API/Puller.cs
+++ Assets/Bermuda/Scripts/BERMUDA/API/Puller.cs
@@ -1,39 +1,45 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using NetMQ;
-// using System;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NetMQ;
+using System;
+using System.Text;
 
-// // Receive data from another sender socket
-// public class Puller : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         AsyncIO.ForceDotNet.Force();
-//         string address = "localhost";
-//         string[] ports = { "5557",
-//             //5558,
-//         };
-//         // Bind to address and port as Puller
-//         using (var receiver = new NetMQ.Sockets.PullSocket("tcp://" + address + ":" + ports[0]))
-//         //using (var sender = new NetMQ.Sockets.PushSocket("tcp://" + address + ":" + ports[1]))
-//         {
-//             string str = "";
-//             // set timeout for receive request
-//             TimeSpan timeout = new TimeSpan(100000000);
-//             // try receive string frame and put it to str
-//             if (receiver.TryReceiveFrameString(timeout, out str))
-//             {
-//                 Debug.Log(str);
-//                 //sender.TrySendFrame(timeout, "World", false);
-//             }
-//         }
-//     }
+// Receive data from another sender socket
+public class Puller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AsyncIO.ForceDotNet.Force();
+        string address = "localhost";
+        string[] ports = { "5557",
+             //5558,
+         };
+        // Bind to address and port as Puller
+        using (var receiver = new NetMQ.Sockets.PullSocket("tcp://" + address + ":" + ports[0]))
+        //using (var sender = new NetMQ.Sockets.PushSocket("tcp://" + address + ":" + ports[1]))
+        {
+            // Init message
+            NetMQ.Msg message = new NetMQ.Msg();
+            message.InitEmpty();
+            // set timeout for receive request
+            TimeSpan timeout = new TimeSpan(100000000);
+            // try receive message and put it to var message
+            if (receiver.TryReceive(ref message, timeout))
+            {
+                // convert message data to string and print
+                Debug.Log(Encoding.UTF8.GetString(message.Data, 0, message.Data.Length));
+                // Send reply message
+                //message.InitGC(Encoding.ASCII.GetBytes("World"), "World".Length);
+                //sender.TrySend(msg: ref message, timeout: timeout, more: false);
+            }
+        }
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-        
-//     }
-// }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
