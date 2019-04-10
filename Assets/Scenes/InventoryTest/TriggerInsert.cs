02f189c482356989d961f18adc6bebe7b6985484
Jose H <13516027@std.stei.itb.ac.id>
2019-04-10 21:37:08 +0700

    Add Inventory insert mechanic

---
+++ Assets/Scenes/InventoryTest/TriggerInsert.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerInsert : MonoBehaviour
+{
+    public UnityEngine.UI.Button button;
+
+    void OnTriggerEnter(Collider other)
+    {
+        button.onClick.AddListener(() => InsertGameObject(other));
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        button.onClick.RemoveListener(() => InsertGameObject(other));
+    }
+
+    void InsertGameObject(Collider other)
+    {
+        other.gameObject.GetComponent<InventoryController>().Insert(gameObject);
+        Destroy(this.gameObject);
+    }
+}
