8d9093915c46d56069673538d81f717854cc5804
Jose H <13516027@std.stei.itb.ac.id>
2019-04-10 16:34:23 +0700

    Add mock datacontainer for UI

---
+++ Assets/Scenes/RoomData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class RoomData
+{
+    public int roomID;
+    public string roomName = "Jax's quest";
+    public List<PlayerData> players; // List of player in the room
+    public bool isFull = false;
+}
---
+++ Assets/Scenes/RoomListData.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class RoomListData
+{
+    public List<RoomData> availableRooms; // List of avaliable rooms that player can join
+}
