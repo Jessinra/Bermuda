0aff670347151cfb7a7ced6b0db8662dbab1a659
JessinRa <jessin.bdg@gmail.com>
2019-04-18 18:09:03 +0700

    Take Fuel done

---
+++ Assets/Bermuda/Scripts/BERMUDA/Interactable/TakeFuel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeFuel : MonoBehaviour {
+    [SerializeField] private Sprite fuelTopEmpty = null;
+    [SerializeField] private Sprite fuelBottomEmpty = null;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+
+        Debug.Log("TODO : Fuel action");
+
+        if (other.CompareTag("Player")) {
+
+            SpriteRenderer renderer = this.transform.parent.gameObject.GetComponent<SpriteRenderer>();
+
+            if (this.gameObject.CompareTag("Fuel Top")) {
+                renderer.sprite = fuelTopEmpty;
+
+            } else if (this.gameObject.CompareTag("Fuel Bottom")) {
+                renderer.sprite = fuelBottomEmpty;
+            }
+
+            Debug.Log("TODO: Increase fuel");
+            // Destroy(this.transform.parent.gameObject);
+        }
+    }
+}
