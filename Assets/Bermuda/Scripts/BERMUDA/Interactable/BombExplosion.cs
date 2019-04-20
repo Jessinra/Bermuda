b80024e4303d75a667762c29f068af870358bdeb
JessinRa <jessin.bdg@gmail.com>
2019-04-20 15:53:31 +0700

    bomb damage player

--- Assets/Bermuda/Scripts/BERMUDA/Interactable/BombExplosion.cs
+++ Assets/Bermuda/Scripts/BERMUDA/Interactable/BombExplosion.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class BombExplosion : MonoBehaviour
 {
+    [SerializeField] private Vector2Int damageRange = new Vector2Int(50, 75);
+    private Player damagedPlayerScript = null;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player"){
+        if (other.CompareTag("Player")) {
 
-            Debug.Log("TODO: reduce player health");
+            if (!damagedPlayerScript) {
+                // Finding the player script
+                damagedPlayerScript = other.gameObject.GetComponent<Player>();
+            }
+
+            // If the script is available, decrease the health
+            if (damagedPlayerScript) {
+
+                int damage = UnityEngine.Random.Range(damageRange.x, damageRange.y);
+                damagedPlayerScript.decreaseHP(damage);
+
+                Debug.Log("TODO : notify server");
+            }
         }
     }
 }
