3c893889bde104dcc4d4a5c327ec2b39e039593d
belladj <belladestianajunaidi@gmail.com>
2019-04-23 15:58:10 +0700

    added hp/fuel bar and popup

---
+++ Assets/Bermuda/Scripts/BERMUDA/UI/PlayerWaitingListController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerWaitingListController : MonoBehaviour
+{
+	public Text roomName;
+	public Text players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roomName.text = "Battle Royale (10/10)";
+        players.text = "";
+
+        for (int i = 0; i < 5; i++) {
+        	players.text += "- Team " + (i+1) + ": Nama (Submarine) & Saya (diver) \n";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Bermuda/Scripts/BERMUDA/UI/PopUpController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpController : MonoBehaviour
+{
+	public Text title;
+	public Text description;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        title.text = "";
+        description.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
