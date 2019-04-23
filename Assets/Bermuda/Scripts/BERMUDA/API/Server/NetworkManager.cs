7bc2a10ef8001a427464e30fc1e5faa380fb7133
Jose H <13516027@std.stei.itb.ac.id>
2019-04-23 17:58:54 +0700

    Add receive info in networkmanager and player's position setter

--- Assets/Bermuda/Scripts/BERMUDA/API/Server/NetworkManager.cs
+++ Assets/Bermuda/Scripts/BERMUDA/API/Server/NetworkManager.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NetMQ;
 using UnityEngine.Networking;
+using System.Threading.Tasks;
 
 public class NetworkManager : MonoBehaviour
 {
     private string URL_SEND = "http://192.168.101.130:5000/api/match/update";
+    private string URL_RECV = "tcp://167.205.34.54:12345";
 
     private string room_id;
+    
+    [SerializeField]private List<Submarine> submarines;
+    [SerializeField]private List<Diver> divers;
 
     [SerializeField] private Player player;
 
     // Start is called before the first frame update
     void Start()
     {
+        // PLayerpref get room_id
         SetRoomId("F6MW2");
         InvokeRepeating("SendInformation", 0.05f, 0.05f);
+        Task.Run(() => ReceiveInformation());
     }
 
     public void SetRoomId(string id)
@@ -79,4 +87,54 @@ public class NetworkManager : MonoBehaviour
         }
         */
     }
+
+    private void ReceiveInformation()
+    {
+        WWW www = new WWW(URL_RECV);   
+        string topic = room_id;
+        
+        using (var subSocket = new NetMQ.Sockets.SubscriberSocket())
+        {
+            subSocket.Options.ReceiveHighWatermark = 1000;
+            subSocket.Connect(URL_RECV);
+            subSocket.Subscribe(topic);
+            //Console.WriteLine("Subscriber socket connecting...");
+            
+            while (true)
+            {
+                var messageTopicReceived = subSocket.ReceiveFrameString();
+                var messageReceived = subSocket.ReceiveFrameString();
+                //Debug.Log(messageReceived);
+                PlayerData[] players = JsonHelper.FromJson<PlayerData>(messageReceived);
+
+                foreach (var playerData in players)
+                {
+                    bool found = false;
+                    for(int i = 0; i < submarines.Count; ++i){
+                        if(submarines[i].GetId() == playerData.id){
+                            submarines[i].SetPositionX(playerData.pos_x);
+                            submarines[i].SetPositionY(playerData.pos_y);
+                            found = true;
+                            break;
+                        }
+                        if (found)
+                            break;
+                    }
+                    for (int i = 0; i < divers.Count && !found; ++i)
+                    {
+                        if (divers[i].GetId() == playerData.id)
+                        {
+                            divers[i].SetPositionX(playerData.pos_x);
+                            divers[i].SetPositionY(playerData.pos_y);
+                            found = true;
+                            break;
+                        }
+                        if (found)
+                            break;
+                    }
+                }
+            }
+        }
+    }
+    //{"players":[{"team":0,"pos_x":0.0,"pos_y":0.0,"id":null,"status":null,"username":"gerywahyu","type":null,"position":null,"angle":0.0}]}
 }
---
+++ Assets/Bermuda/Scripts/BERMUDA/API/Server/TestJsonHelper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class TestJsonHelper : MonoBehaviour
+{
+    PlayerData[] players;
+    string json;
+    private void Start()
+    {
+        players = new PlayerData[2];
+        players[0] = new PlayerData
+        {
+            id = "0",
+            pos_x = 24,
+            pos_y = 52
+        };
+        players[1] = new PlayerData
+        {
+            id = "1",
+            pos_x = 17,
+            pos_y = 48
+        };
+        json = JsonHelper.ToJson<PlayerData>(players, false);
+        Debug.Log(json);
+        players[0].id = "ss";
+        players[0].pos_x = 25;
+        players[1].id = "sst";
+        players = JsonHelper.FromJson<PlayerData>(json);
+        Debug.Log(players[0].pos_x);
+        Debug.Log(players[1].id);
+    }
+}
