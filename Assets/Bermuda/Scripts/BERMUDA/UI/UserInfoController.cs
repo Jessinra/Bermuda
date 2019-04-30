42984ac46c080c46bf6a3ff73e3a1e6b9fd005ec
JessinRa <jessin.bdg@gmail.com>
2019-04-30 22:41:33 +0700

    Integrate with PlayerInfo UI

--- Assets/Bermuda/Scripts/BERMUDA/UI/UserInfoController.cs
+++ Assets/Bermuda/Scripts/BERMUDA/UI/UserInfoController.cs
@@ -3,42 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UserInfoController : MonoBehaviour
-{
+public class UserInfoController : MonoBehaviour {
 
-	public GameObject player;
-	public Slider HP;
-	public Slider Fuel;
-	public Sprite char1;
-	public Sprite char2;
-	public Sprite char3;
-	public Text playerName;
-	public Image character;
-	int charID = 2;
+	[SerializeField] private Slider sliderHP = null;
+	[SerializeField] private Slider sliderFuel = null;
 
+	[SerializeField] private List<Sprite> characterThumbnail = new List<Sprite>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerName.text = "insert name";
-        HP.value = 100;
-        Fuel.value = 100;
-        
-        if (charID == 1 ) {
-        		character.sprite = char1;
-        	}
-        	else if (charID == 2 ) {
-        		character.sprite = char2;
-        	}
-        	else {
-        		character.sprite = char3;
-        	}
+	[SerializeField] private Text uiPlayerName = null;
+	[SerializeField] private Image uiPlayerThumbnail = null;
 
-    }
+	private Player playerScript = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Start() {
+		StartCoroutine(SetupUserProfile());
+	}
+
+	IEnumerator SetupUserProfile() {
+
+		while (playerScript == null) {
+			yield return new WaitForSeconds(0.1F);
+			playerScript = this.transform.parent.gameObject.GetComponent<Player>();
+		}
+
+		uiPlayerName.text = playerScript.GetUsername();
+		uiPlayerThumbnail.sprite = characterThumbnail[playerScript.GetPlayerType() - 1];
+		
+		StartCoroutine(GetUserStatus());
+		yield break;
+	}
+
+	IEnumerator GetUserStatus() {
+
+		while (playerScript != null) {
+			sliderHP.value = playerScript.GetHP();
+			sliderFuel.value = playerScript.GetFuel();
+
+			yield return new WaitForSeconds(0.2F);
+		}
+
+		yield break;
+	}
 }
